db742e6917db7ffad5af27d03e49ef5dc3c6c6f2
Yerson Reyes <yerson.programador@gmail.com>
2022-05-20 11:16:24 -0400

    primer commit

---
+++ src/views/Buscador.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="formulario">
+    <h1>Buscador de peleadores</h1>
+    <form @submit.prevent="handleSubmit">
+      <input
+        type="text"
+        v-model="idBuscar"
+        required
+        minlength="3"
+        maxlength="3"
+      />
+      <button type="submit">Buscar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    idBuscar: "",
+  }),
+  methods: {
+    handleSubmit() {
+      this.$router.push(`/peleadores/${this.idBuscar}`);
+    },
+  },
+};
+</script>
+
+<style>
+.formulario{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
---
+++ src/views/Peleador.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="peleador_container">
+    <div class="peleador" v-if="peleador">
+      <h2>Nombre: {{ peleador.nombre }}</h2>
+      <h4>Id: {{ $route.params.id }}</h4>
+      <img :src="peleador.imgSrc" alt="" width="100px" height="100px" />
+    </div>
+
+    <div v-if="!peleador">
+      <h1>No se pudo encontrar el peleador =(</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    peleador: { nombre: "", imgSrc: "" },
+  }),
+  created() {
+    fetch("/peleadores.json")
+      .then((response) => response.json())
+      .then((peleadores) => {
+        this.peleador =
+          peleadores.find(
+            (peleador) => peleador.id === this.$route.params.id
+          ) || null;
+      });
+  },
+};
+</script>
+
+<style>
+
+.peleador{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
